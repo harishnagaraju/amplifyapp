e681a22813115fde1085975dee2b3bd3c8e95df9
analystcode <analystcode@gmail.com>
2021-04-07 23:29:11 +0530

    Testing a form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+</script>
+</head>
+<body>
+
+<form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
+  Name: <input type="text" name="fname">
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
+
+
+
 function App() {
   return (
     <div className="App">
