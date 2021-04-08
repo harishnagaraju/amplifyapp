1b5f09e824ea23c9e4ff528bca570291b31ce152
analystcode <analystcode@gmail.com>
2021-04-08 11:46:18 +0530

    app.js is back to old code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-
-
-
 function App() {
   return (
-    <!DOCTYPE html>
-<html>
-<head>
-<script>
-function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
-</script>
-</head>
-<body>
-
-<form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
-  Name: <input type="text" name="fname">
-  <input type="submit" value="Submit">
-</form>
-</body>
-</html>
-    
-    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
